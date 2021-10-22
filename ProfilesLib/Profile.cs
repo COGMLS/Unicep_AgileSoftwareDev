2a08f9a9e6f51e33e1891f93d49ce7933b83b8eb
COGMLS <mlsmatheus@hotmail.com>
2021-10-22 15:52:16 -0300

    Realizadas pequenas otimizações no código para reduzir o tempo de acesso aos dados da classe.

--- ProfilesLib/Profile.cs
+++ ProfilesLib/Profile.cs
@@ -9,43 +9,52 @@ namespace ProfilesLib
 {
 	public class Profile
 	{
+		//Profile name
 		private string ProfileName;
 
+		//Constants to control the profile directory
 		readonly private string AutoStartAppsBase = "Auto Start Apps";
 		readonly private string ProfilesBase = "Profiles";
 		readonly private string ProfileFolderExt = ".profile";
 		readonly private string ProfileInternal_Init = "Init";
 		readonly private string ProfileInternal_Estatistics = "PerfAnalyze";
 
+		//Directory to AppData and Profile folder:
 		private string LocalAppDataEnv;
+		private string ProfileContainer;
 
+		//Variable control.
 		private bool ProfileDirExist;
+
+		//Initialization List:
+		InitializationList ProfileInitList = null;
 
 		Profile(string profileName)
 		{
 			this.ProfileName = profileName;
 			this.LocalAppDataEnv = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+			this.ProfileDirExist = Directory.Exists(this.LocalAppDataEnv + "\\" + this.AutoStartAppsBase + "\\" + this.ProfilesBase + "\\" + ProfileName + this.ProfileFolderExt);
 		}
 
 		//Load profile, selected by the name.
 		public int LoadProfile(string ProfileName)
 		{
-			string ProfileDir = this.LocalAppDataEnv + "\\" + this.AutoStartAppsBase + "\\" + this.ProfilesBase + "\\" + ProfileName + this.ProfileFolderExt;
-			
 			//Verify if Profile directory exist.
-			if(Directory.Exists(ProfileDir))
+			if(this.ProfileDirExist)
 			{
 				//Get the Initialization files.
-				string[] ProfileInitFilesList = Directory.GetFiles(ProfileDir);
+				string[] ProfileInitFilesList = Directory.GetFiles(this.ProfileContainer + "\\" + this.ProfileInternal_Init);
 				//Get the Metrics files.
-				string[] ProfileMetricsFilesList = Directory.GetFiles(ProfileDir);
+				string[] ProfileMetricsFilesList = Directory.GetFiles(this.ProfileContainer + "\\" + this.ProfileInternal_Estatistics);
 
 				if(ProfileInitFilesList.Length > 0)
 				{
-					InitializationList ProfileInitList = new InitializationList(ProfileInitFilesList.Length);
+					//Initilize the ProfileInitList
+					ProfileInitList = new InitializationList(ProfileInitFilesList.Length);
 
 					for(int i = 0; i < ProfileInitFilesList.Length; i++)
-                    {
+					{
 						string[] LoadedProgramConfigs = File.ReadAllLines(ProfileInitFilesList[i]);
 
 						string ProgramName = null;
@@ -58,7 +67,7 @@ namespace ProfilesLib
 
 						for (int j = 0; j < LoadedProgramConfigs.Length; j++)
 						{
-
+							//Gets the configurations saved, searching for the right tags.
 							if (ProfileConfig.IsConfigTag(ProfileConfig.ConfigTags.PROGRAMNAME, ref ProfileInitFilesList[i]))
 							{
 								ProgramName = ProfileConfig.LoadConfigString(ProfileConfig.ConfigTags.PROGRAMNAME, ref ProfileInitFilesList[i]);
@@ -89,14 +98,15 @@ namespace ProfilesLib
 							}
 						}
 
+						//If the working directory is null
 						if (WorkingDir == null)
 						{
-							ProfileInitList.Add2Init(ProgramName, CmdLine, Args, WaitTime);
+							_ = ProfileInitList.Add2Init(ProgramName, CmdLine, Args, WaitTime, Priority, windowStyle);
 						}
 						else
-                        {
-							ProfileInitList.Add2Init(ProgramName, CmdLine, Args, WaitTime, WorkingDir, Priority, windowStyle);
-                        }
+						{
+							_ = ProfileInitList.Add2Init(ProgramName, CmdLine, Args, WaitTime, WorkingDir, Priority, windowStyle);
+						}
 					}
 
 					return 0;
@@ -117,6 +127,26 @@ namespace ProfilesLib
 		}
 
 		//Save the profile
+		public void SaveProfile(bool OverWrite)
+        {
+			string ProfileInitConfig = this.ProfileContainer + "\\" + this.ProfileInternal_Init;
+
+			for (int i = 0; i < this.ProfileInitList.GetInitSize(); i++)
+            {
+				string[] LoadedProgramConfigs;
+
+				//string ProgramName = null;
+				//string CmdLine = null;
+				//string Args = null;
+				//string WorkingDir = null;
+				//StartWindowStyle windowStyle = StartWindowStyle.NORMAL;
+				//CommonTypes.StartPriority Priority = CommonTypes.StartPriority.NORMAL;
+				//int WaitTime = 0;
+
+				
+				
+			}
+        }
 		public void SaveProfile(string ProfileName, bool OverWrite)
 		{
 
@@ -125,17 +155,17 @@ namespace ProfilesLib
 		//Verify the profiles available
 		string[] GetProfiles()
 		{
-			string ProfileDir = this.LocalAppDataEnv + "\\" + this.AutoStartAppsBase + "\\" + this.ProfilesBase;
+			string ProfilesDir = this.LocalAppDataEnv + "\\" + this.AutoStartAppsBase + "\\" + this.ProfilesBase;
 
 			if (AuxiliarProfileManager.ProfileDirExist())
-            {
-				int NumProfiles = AuxiliarProfileManager.GetProfilesList(ProfileDir).Length;
+			{
+				int NumProfiles = AuxiliarProfileManager.GetProfilesList(ProfilesDir).Length;
 
-            }
+			}
 			else
-            {
+			{
 
-            }
+			}
 
 
 			string[] ProfilesList = new string[10];
@@ -143,253 +173,6 @@ namespace ProfilesLib
 
 
 			return ProfilesList;
-		}
-
-
-	}
-
-	public static class AuxiliarProfileManager
-    {
-		readonly static private string AutoStartAppsBase = "Auto Start Apps";
-		readonly static private string ProfilesBase = "Profiles";
-		readonly static private string ProfileFolderExt = ".profile";
-
-		//Check the profile directory
-		public static bool ProfileDirExist()
-        {
-			string LocalAppDataEnv = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			string ProfilesBaseDir = LocalAppDataEnv + "\\" + AutoStartAppsBase + "\\" + ProfilesBase;
-
-			//Verify if Profile directory exist.
-			if (Directory.Exists(ProfilesBaseDir))
-			{
-				return true;
-			}
-			else
-            {
-				return false;
-            }
-		}
-
-		//Get the Enumerated profiles available.
-		public static string[] GetProfilesList(string Path)
-        {
-			//Verify if Profile directory exist.
-			if (Directory.Exists(Path))
-			{
-				//Get the number of profiles.
-				string[] ProfilePreLoad = Directory.GetDirectories(Path);
-
-				if (ProfilePreLoad.Length > 0)
-				{
-					//Get the files list with a full paths.
-					string[] ProfilesList = Directory.GetDirectories(Path);
-					int PathCut = Path.Length + 1;
-
-					//Verify if the FilesList has some file(s) to treat.
-					if (ProfilesList.Length != 0)
-					{
-						string[] FinalFilesList = new string[ProfilesList.Length];
-
-						for (int i = 0; i < ProfilesList.Length; i++)
-						{
-							FinalFilesList[i] = ProfilesList[i].Substring(PathCut);
-						}
-
-						//Return the names list array.
-						return FinalFilesList;
-					}
-					else
-					{
-						//Return a NULL value in case a zeroed list.
-						return null;
-					}
-				}
-				else
-				{
-					//Profile dosn't have profiles to be loaded.
-					return null;
-				}
-			}
-			else
-			{
-				//Profile base folder dosn't exist.
-				return null;
-			}
-		}
-
-		//Get the number of profiles available for the user
-		public static int NumProfiles(string Path)
-        {
-			var EnumDirs = Directory.EnumerateDirectories(Path);
-
-			int NumProfiles = 0;
-
-            foreach (var item in EnumDirs)
-            {
-				if(item.EndsWith(".profile"))
-                {
-					NumProfiles++;
-                }
-            }
-
-			return NumProfiles;
-        }
-
-		//Create base profile directory with calling the directory class and handles with exceptions. If create with sucess the return will be a NULL value.
-		static System.Exception CreateProfileRepos()
-        {
-			string LocalAppDataEnv = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-			string ProfilesBaseDir = LocalAppDataEnv + "\\" + AutoStartAppsBase + "\\" + ProfilesBase;
-
-			try
-			{
-				Directory.CreateDirectory(ProfilesBaseDir);
-				return null;
-			}
-			catch (Exception exception)
-			{
-				return exception;
-				throw;
-			}
-		}
-    }
-
-	public static class ProfileConfig
-    {
-		readonly private static string[] ConfigNames =
-		{
-			"NAME=",
-			"CMD=",
-			"ARGS=",
-			"WORKDIR=",
-			"WINSTYLE=",
-			"PRIORITY=",
-			"WAIT="
-		};
-
-        public enum ConfigTags : int
-        {
-			PROGRAMNAME,	//String
-			COMMANDLINE,	//String
-			ARGS,			//String
-			WORKINGDIR,		//String
-			WINDOWSTYLE,	//Enum
-			PRIORITY,		//Enum
-			WAITTIME		//Int
-        }
-
-		public static bool IsConfigTag(ConfigTags CfgTag, ref string ConfigString)
-        {
-			if(ConfigString.StartsWith(ConfigNames[(int)CfgTag]))
-            {
-				return true;
-            }
-			else
-            {
-				return false;
-            }
-        }
-
-		//Load the respectly config
-		public static int LoadConfig(ConfigTags CfgTag, ref string ConfigString, ref object obj)
-        {
-			if(CfgTag == ConfigTags.PROGRAMNAME || CfgTag == ConfigTags.COMMANDLINE || CfgTag == ConfigTags.ARGS || CfgTag == ConfigTags.WORKINGDIR)
-            {
-				obj = LoadConfigString(CfgTag, ref ConfigString);
-
-				return 0;
-            }
-			else if(CfgTag == ConfigTags.WINDOWSTYLE)
-            {
-				obj = LoadConfigWinStyle(CfgTag, ref ConfigString);
-
-				return 0;
-            }
-			else if(CfgTag == ConfigTags.PRIORITY)
-            {
-				obj = LoadConfigPriority(CfgTag, ref ConfigString);
-
-				return 0;
-            }
-			else if(CfgTag == ConfigTags.WAITTIME)
-            {
-				obj = LoadConfigInt(CfgTag, ref ConfigString);
-
-				return 0;
-            }
-			else
-            {
-				obj = null;
-
-				return -1;
-            }
-        }
-
-		//Load and treat correctly the string value
-		public static string LoadConfigString(ConfigTags CfgTag, ref string ConfigString)
-		{
-			return ConfigString.Substring(ConfigNames[(int)CfgTag].Length);
-		}
-
-		//Load and treat correctly the StartWindowStyle Enum value
-		public static StartWindowStyle LoadConfigWinStyle(ConfigTags CfgTag, ref string ConfigString)
-		{
-			string t = ConfigString.Substring(ConfigNames[(int)CfgTag].Length);
-
-			if(t == "-1")
-            {
-				return StartWindowStyle.NOWINDOW;
-            }
-			else if(t == "0")
-            {
-				return StartWindowStyle.NORMAL;
-            }
-			else if(t == "1")
-            {
-				return StartWindowStyle.HIDDEN;
-            }
-			else if(t == "2")
-            {
-				return StartWindowStyle.MINIMIZED;
-            }
-			else if(t == "3")
-            {
-				return StartWindowStyle.MAXIMIZED;
-            }
-			else
-            {
-				return StartWindowStyle.NORMAL;
-            }
-		}
-
-		//Load and treat correctly the integer value
-		public static int LoadConfigInt(ConfigTags CfgTag, ref string ConfigString)
-		{
-			return int.Parse(ConfigString.Substring(ConfigNames[(int)CfgTag].Length));
-		}
-
-		//Load and treat correctly the StartPriority Enum value
-		public static CommonTypes.StartPriority LoadConfigPriority(ConfigTags CfgTag, ref string ConfigString)
-		{
-			string t = ConfigString.Substring(ConfigNames[(int)CfgTag].Length);
-
-			if(t == "0")
-            {
-				return CommonTypes.StartPriority.LOW;
-            }
-			else if(t == "1")
-            {
-				return CommonTypes.StartPriority.NORMAL;
-            }
-			else if(t == "2")
-            {
-				return CommonTypes.StartPriority.HIGH;
-            }
-			else
-            {
-				return CommonTypes.StartPriority.NORMAL;
-            }
 		}
 	}
 }
