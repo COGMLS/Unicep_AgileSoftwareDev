262177c6faea2c9b69894b5c2694c992304cffe5
COGMLS <mlsmatheus@hotmail.com>
2021-11-01 22:57:17 -0300

    Criada a função responsável pelo timer e inicialização do objeto.

--- ProcessAuxLib/ProcessAuxLib.cs
+++ ProcessAuxLib/ProcessAuxLib.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
-using System.Threading;
+
 using System.Timers;
 
 namespace ProcessAuxLib
 {
-    public class ProcessAuxLib
+    public static class ProcessAux
     {
+        public static float MakeProcess(ref ProcessStartInfo ProcessObj)
+        {
+            try
+            {
+                //Start timer
+                Timer t = new Timer();
+                t.Start();
 
+                //Start process
+                Process.Start(ProcessObj);
+
+                //Stop timer
+                t.Stop();
+
+                double d = t.Interval;
+
+                return (float)d;
+            }
+            catch(Exception)
+            {
+                return -1.0f;
+            }
+        }
     }
 }
