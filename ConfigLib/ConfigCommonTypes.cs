504b43495191f4de0e0ff2f41a988b423ad5ed2f
COGMLS <mlsmatheus@hotmail.com>
2021-11-18 16:28:17 -0300

    Criada classe estática ConfigLib para dados comuns

---
+++ ConfigLib/ConfigCommonTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConfigLib
+{
+    public static class ConfigCommonTypes
+    {
+        //
+        //Constants:
+        //
+        public static readonly string CommonConfigPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\" + "Auto Start Apps";
+
+    }
+}
