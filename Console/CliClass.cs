fedeafbb796155730d3f2b7c9b82054aeca443b3
COGMLS <mlsmatheus@hotmail.com>
2021-11-19 17:34:19 -0300

    Adicionado ao enumerador REMAPP Alterada a matriz CL_ARRAY para aceitar --RemApp Modificados os argumentos. Alterados os valores de peso da linha de comando. Adaptadas todas as funções da linha de comando para receber RemApp

--- Console/CliClass.cs
+++ Console/CliClass.cs
@@ -11,6 +11,7 @@ namespace AutoStartupConsole
 			REMOVEPROFILE,
 			LISTPROFILE,
 			ADD2PROFILE,
+			REMAPP,
 			ADDCSV2PROFILE,
 			ADMINCONFIG,
 			ADMINPASS
@@ -24,21 +25,23 @@ namespace AutoStartupConsole
 			"--RemoveProfile",		//[2] Remove and delete a profile (needs --AdminPass)
 			"--ListProfiles",		//[3] List the profiles available for the user account (for all users)
 			"--Add2Profile",		//[4] Adds a new application to start initialization list (needs --AdminPass)
-			"--AddCsv2Profile",		//[5] Adds a CSV file with each line as a app entry to add in init. list. The sequence of values needs be ProgramName, CmdLine, Args, WaitTime, WorkingDir*, Priority2Start, WindowStyle
-			"--AdminConfig",		//[6] Changes the AdminPass, if AdminPass isn't setted, the AdminPass will be avoided (needs --AdminPass)
-			"--AdminPass",			//[7] Administrator Password
+			"--RemApp",				//[5] Removes a application registered in a profile (needs --AdminPass)
+			"--AddCsv2Profile",		//[6] Adds a CSV file with each line as a app entry to add in init. list. The sequence of values needs be ProgramName, CmdLine, Args, WaitTime, WorkingDir*, Priority2Start, WindowStyle
+			"--AdminConfig",		//[7] Changes the AdminPass, if AdminPass isn't setted, the AdminPass will be avoided (needs --AdminPass)
+			"--AdminPass",			//[8] Administrator Password
 		};
 
 		/** Sum of return value available:
 		 * ----------------------------------------------------
 		 * LoadProfile - 1
-		 * NewProfile - 2 (Needs AdminPass) -> 130
-		 * RemoveProfile - 4 (Needs AdminPass) -> 132
+		 * NewProfile - 2 (Needs AdminPass) -> 258
+		 * RemoveProfile - 4 (Needs AdminPass) -> 260
 		 * ListProfile - 8
-		 * Add2Profile - 16 (Needs AdminPass) -> 144
-		 * AddCsv2Profile - 32 (Needs AdminPass) -> 160
-		 * AdminConfig - 64 (Needs AdminPass) -> 192
-		 * AdminPass - 128 (Needs be used with another parameter)
+		 * Add2Profile - 16 (Needs AdminPass) -> 272
+		 * RemApp - 32 (Needs AdminPass) -> 288
+		 * AddCsv2Profile - 64 (Needs AdminPass) -> 320
+		 * AdminConfig - 128 (Needs AdminPass) -> 384
+		 * AdminPass - 256 (Needs be used with another parameter)
 		 */
 
 		//Converts the enumerator to a integer value
@@ -56,12 +59,14 @@ namespace AutoStartupConsole
 					return 3;
                 case CommandLineAvailable.ADD2PROFILE:
 					return 4;
-                case CommandLineAvailable.ADDCSV2PROFILE:
+				case CommandLineAvailable.REMAPP:
 					return 5;
-                case CommandLineAvailable.ADMINCONFIG:
+                case CommandLineAvailable.ADDCSV2PROFILE:
 					return 6;
-                case CommandLineAvailable.ADMINPASS:
+                case CommandLineAvailable.ADMINCONFIG:
 					return 7;
+                case CommandLineAvailable.ADMINPASS:
+					return 8;
 				default:
 					return 0;
             }
@@ -83,10 +88,12 @@ namespace AutoStartupConsole
                 case 4:
 					return CommandLineAvailable.ADD2PROFILE;
                 case 5:
+					return CommandLineAvailable.REMAPP;
+				case 6:
 					return CommandLineAvailable.ADDCSV2PROFILE;
-                case 6:
-					return CommandLineAvailable.ADMINCONFIG;
                 case 7:
+					return CommandLineAvailable.ADMINCONFIG;
+                case 8:
 					return CommandLineAvailable.ADMINPASS;
 				default:
 					return CommandLineAvailable.LOADPROFILE;
@@ -164,7 +171,7 @@ namespace AutoStartupConsole
 					{
 						return false;
 					}
-				case 32:		//--AddCsv2Profile (Ok if dosn't have a password setted)
+				case 32:        //--RemApp (Ok if dosn't have a password setted)
 					if (IsAdminPassSetted)
 					{
 						return true;
@@ -173,19 +180,30 @@ namespace AutoStartupConsole
 					{
 						return false;
 					}
-				case 64:		//--AdminConfig
+				case 64:		//--AddCsv2Profile (Ok if dosn't have a password setted)
+					if (IsAdminPassSetted)
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				case 128:		//--AdminConfig
 					return false;
-				case 128:		//--AdminPass
+				case 256:		//--AdminPass
 					return false;
-				case 130:		//--NewProfile --AdminPass
+				case 258:		//--NewProfile --AdminPass
 					return true;
-				case 132:		//--RemoveProfile --AdminPass
+				case 260:		//--RemoveProfile --AdminPass
 					return true;
-				case 144:		//--Add2Profile --AdminPass
+				case 272:		//--Add2Profile --AdminPass
 					return true;
-				case 160:		//--AddCsv2Profile --AdminPass
+				case 288:		//--RemApp --AdminPass
 					return true;
-				case 192:		//--AdminConfig --AdminPass
+				case 320:		//--AddCsv2Profile --AdminPass
+					return true;
+				case 384:		//--AdminConfig --AdminPass
 					return true;
 				default:		//Any other combination will be considered not available to work
 					return false;
@@ -204,7 +222,7 @@ namespace AutoStartupConsole
 				if (Args[i] == CL_ARRAY[Enum2Int(IndexE)])
                 {
 					//Check if the command will receave a value or not, using the ClWeightSum and IsCmdLineOk control variables
-					if ((ClWeightSum >= 130 || (ClWeightSum >= 1 && ClWeightSum <= 32)) && IsCmdLineOk == true)
+					if ((ClWeightSum >= 258 || (ClWeightSum >= 1 && ClWeightSum <= 64)) && IsCmdLineOk == true)
 					{
 						//Verify if has another element to be readed and loaded to the return variable
 						if (i + 1 < Args.Length)
